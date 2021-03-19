65b77af30248d74c13d6bab685c23f4528055035
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-19 15:45:07 -0400

    Zach's changes 1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 let inputForm = document.querySelector("form#create-task-form")
+let unorderedList = document.querySelector("#tasks")
 
- inputForm.addEventListener("click",function(evt){
+ inputForm.addEventListener("submit", function(evt){
   evt.preventDefault()
+
+  let removeButton = document.createElement("button")
+  removeButton.innerText = "X"
   let theForm = evt.target
   let theInput = theForm["new-task-description"]
+  // console.log(theInput)
+  // console.log(evt.target)
+  // debugger
+  let userInput = theInput.value
 
- })
+  let blankLi = document.createElement('li')
+  blankLi.innerText = userInput
+  unorderedList.append(blankLi)
+  unorderedList.append(removeButton)
+  // userInput.remove()
+  evt.target.reset()
+  // renderApp();
+  removeButton.addEventListener("click", function () {
+    removeButton.remove()
+    blankLi.remove()
+
+  })
+
+  // inputForm.addEventListener("click")
+})
