387c2c228128d3706b3db400bd729567679dba43
petercrawford <pcraw1027@gmail.com>
2021-03-19 15:02:16 -0400

    Peter to Zach 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
 });
+
+let inputForm = document.querySelector("form#create-task-form")
+
+ inputForm.addEventListener("click",function(evt){
+  evt.preventDefault()
+  let theForm = evt.target
+  let theInput = theForm["new-task-description"]
+
+ })
